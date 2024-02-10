2048448d8ed3ba6957b339e54577b9363203367e
gio-del <giovanni.delucia@polimi.it>
2024-02-10 15:24:49 +0100

    contact box

--- src/components/contact_section/Contact.tsx
+++ src/components/contact_section/Contact.tsx
@@ -8,14 +8,14 @@ export default function Contact(props: ContactProps) {
   const { contact } = props;
 
   return (
-    <div className="flex lg:space-x-28 space-x-10 justify-center items-center">
+    <div className="flex lg:space-x-28 space-x-10 justify-center items-center border-t-2 border-b-2 border-secondary dark:border-dk-secondary py-10 hover:border-accent dark:hover:border-dk-accent">
       <a
         href={`mailto:${contact.email}`}
         target="_blank"
         rel="noreferrer"
         className="text-secondary dark:text-dk-secondary"
       >
-        <i className="fas fa-envelope lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent"></i>
+        <i className="fas fa-envelope lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent hover:animate-spin"></i>
       </a>
       <a
         href={contact.github}
@@ -23,7 +23,7 @@ export default function Contact(props: ContactProps) {
         rel="noreferrer"
         className="text-secondary dark:text-dk-secondary"
       >
-        <i className="fab fa-github lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent"></i>
+        <i className="fab fa-github lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent hover:animate-spin"></i>
       </a>
       <a
         href={contact.linkedin}
@@ -31,7 +31,7 @@ export default function Contact(props: ContactProps) {
         rel="noreferrer"
         className="text-secondary dark:text-dk-secondary"
       >
-        <i className="fab fa-linkedin lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent"></i>
+        <i className="fab fa-linkedin lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent hover:animate-spin"></i>
       </a>
       <a
         href={contact.twitter}
@@ -39,7 +39,7 @@ export default function Contact(props: ContactProps) {
         rel="noreferrer"
         className="text-secondary dark:text-dk-secondary"
       >
-        <i className="fab fa-x-twitter lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent"></i>
+        <i className="fab fa-x-twitter lg:text-9xl text-5xl hover:text-accent dark:hover:text-dk-accent hover:animate-spin"></i>
       </a>
     </div>
   );
